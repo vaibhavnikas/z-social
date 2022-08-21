eda0164c34807359168270c830e7261846f534fa
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-08-21 23:52:53 +0530

    set up port from environment variables

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const app = express();
 require('./config/view-helpers')(app);
-const port = 8000;
+const port = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
